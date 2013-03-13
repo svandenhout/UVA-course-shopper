321c7f48a915297209fe0fc79a981e56965f8342
steven <stevenvandenhout@gmail.com>
2013-03-13 14:56:14 +0100

    initial rials install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project2::Application.config.secret_token = '3a580eda88c5c0c84bba33ac2058f4c0d9cece81bb47042faecaa9a0943b6d1db1d82e1f879992a66000524d0d5eb53c457fbfbf6e066fa06472a9964b77ef80'
